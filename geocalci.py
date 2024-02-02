4cd8fc3cced1068aeebada2ccd89c88e7b45de13
viswa teja <dakarajuviswa@gmail.com>
2024-02-02 22:10:32 +0530

    geocalci

---
+++ geocalci.py
@@ -0,0 +1,15 @@
+import math
+
+class GeometryCalculator:
+
+def calculate_circle_area(self, radius):
+
+return math.pi * radius ** 2
+
+def calculate_rectangle_area(self, length, width):
+
+return length * width
+
+if __name__ == "__main__":
+
+calculator = GeometryCalculator()
